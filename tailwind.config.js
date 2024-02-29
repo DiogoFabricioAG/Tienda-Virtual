e71ea5fc61e054d39b623b64f2a6323e0adff2f8
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2024-02-28 23:57:35 -0500

    Creacion del Proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      "merriweather":["Merriweather", 'serif'],
+      "josefin":["Josefin Sans", 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
+
