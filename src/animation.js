733b6352f514647402450e82ad40001b593c7a10
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2024-03-01 12:30:27 -0500

    Proceso

---
+++ src/animation.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var elemento = document.getElementById("animacion");
+    elemento.classList.remove("opacity-0", "-translate-y-full");
+    elemento.classList.add("transition-transform", "duration-500", "ease-in-out", "delay-300", "translate-y-0", "opacity-100");
+});
