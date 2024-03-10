a109ccc09299432c762e456564b66791c5137c12
Diogo <diogo.abregu.g@uni.pe>
2024-03-10 00:04:25 -0500

    add: iframe youtube video

--- src/animation.js
+++
@@ -1,6 +0,0 @@
-const ids = []
-document.addEventListener("DOMContentLoaded", function() {
-    var elemento = document.getElementById("animacion");
-    elemento.classList.remove("opacity-0", "-translate-y-full");
-    elemento.classList.add("transition-transform", "duration-500", "ease-in-out", "delay-300", "translate-y-0", "opacity-100");
-});
